082d4fcb5a1ecb401167e42afe9c37996a92292d
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2020-07-12 11:06:53 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
